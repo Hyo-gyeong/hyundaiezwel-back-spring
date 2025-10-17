a45b8f36401be86c0c402648e6e9cb187b5c3b6e
hyogyeong <cdkrcd8@gmail.com>
2025-10-17 10:17:11 +0900

    component 생성

---
+++ aop_01/src/main/java/aop_01/com/spring_aop/xml/Gugudan.java
@@ -0,0 +1,21 @@
+package aop_01.com.spring_aop.xml;
+
+import org.springframework.stereotype.Component;
+
+public class Gugudan { // ÇÙ½É ±â´É
+	private int dan;
+
+	public int getDan() {
+		return dan;
+	}
+
+	public void setDan(int dan) {
+		this.dan = dan;
+	}
+	
+	public void showResult() {
+		for (int i = 1; i <= 9; i++) {
+			System.out.println(dan+"*"+i+"="+(dan*i));
+		}
+	}
+}
